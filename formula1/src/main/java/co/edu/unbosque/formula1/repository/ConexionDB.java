763b51a2f7ef0b465453e5f902d71b8f4468ba08
sofil <sofil@172.20.36.228>
2025-10-24 16:34:54 -0500

    Clase de conexión a la base de datos

---
+++ formula1/src/main/java/co/edu/unbosque/formula1/repository/ConexionDB.java
@@ -0,0 +1,19 @@
+package co.edu.unbosque.formula1.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConexionDB {
+
+	private static final String URL = "jdbc:mysql://172.20.36.228:3306/formula1";
+	private static final String USURARIO= "sofi";
+	private static final String CONTRASENIA = "formula1";
+	
+	public Connection obtenerConexion () throws SQLException {
+		return DriverManager.getConnection(URL,USURARIO,CONTRASENIA);
+	}
+}
