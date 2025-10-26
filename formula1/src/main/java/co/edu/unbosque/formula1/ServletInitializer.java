ae127f3aedc7a87e4995987c069b367be4d5ebc5
lintim124 <lmninot@unbosque.edu.co>
2025-10-25 20:19:29 -0500

    commit2

---
+++ formula1/src/main/java/co/edu/unbosque/formula1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.formula1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Formula1Application.class);
+	}
+
+}
