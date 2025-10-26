c023b44a578725682803017473122cae499c1bc0
lintim124 <lmninot@unbosque.edu.co>
2025-10-25 20:37:00 -0500

    clase

--- formula1/src/main/java/co/edu/unbosque/formula1/repository/ConexionDB.java
+++ formula1/src/main/java/co/edu/unbosque/formula1/repository/ConexionDB.java
@@ -1,4 +1,3 @@
-
 package co.edu.unbosque.formula1.repository;
 
 import java.sql.Connection;
@@ -8,34 +7,15 @@ import java.sql.SQLException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConexionDB {
+	public class ConexionDB {
 
-	private static final String URL = "jdbc:mysql://172.20.36.228:3306/formula1";
-	private static final String USURARIO= "sofi";
-	private static final String CONTRASENIA = "formula1";
-	
-	public Connection obtenerConexion () throws SQLException {
-		return DriverManager.getConnection(URL,USURARIO,CONTRASENIA);
+		private static final String URL = "jdbc:mysql://192.168.20.36:3306/formula1";
+		private static final String USURARIO= "sofi";
+		private static final String CONTRASENIA = "formula1";
+		
+		public Connection obtenerConexion () throws SQLException {
+			return DriverManager.getConnection(URL,USURARIO,CONTRASENIA);
+		}
 	}
-}
-=======
-package co.edu.unbosque.formula1.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ConexionDB {
-
-	private static final String URL = "jdbc:mysql://192.168.20.36:3306/formula1";
-	private static final String USURARIO= "sofi";
-	private static final String CONTRASENIA = "formula1";
-	
-	public Connection obtenerConexion () throws SQLException {
-		return DriverManager.getConnection(URL,USURARIO,CONTRASENIA);
-	}
-}
 
